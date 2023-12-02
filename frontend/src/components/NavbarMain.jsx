dcc993835943a75c4e76ae50d376f0ec4f3c1b07
piumi <piyumikavindyappk@gmail.com>
2023-12-02 12:36:57 +0530

    updated

--- frontend/src/components/NavbarMain.jsx
+++ frontend/src/components/NavbarMain.jsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
-// import '../styles/NavbarMain.css';
+import '../styles/NavbarMain.css';
+import "../styles/AdminHome.css";
 
 export default function NavbarMain({ isAuthenticated, handleSignOut, loggedInUser }) {
   const [user, setUser] = useState({});
