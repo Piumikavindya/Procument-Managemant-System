b7b672b7043d2a2ec924f67eb4f2b817c7b0b27b
piumi <piyumikavindyappk@gmail.com>
2023-12-03 09:29:07 +0530

    crete manage suppliers

---
+++ frontend/src/pages/AddVenders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddVenders() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ frontend/src/pages/DeleteVendors.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DeleteVendors() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
