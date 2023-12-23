c38d2aa62b091b1c8c91b785b29de5ceabad0ccf
H-Neethika <neethikahariharasakthy2000@gmail.com>
2023-12-23 21:08:32 +0530

    Footer is completed

--- frontend/src/components/CommonFooter.jsx
+++ frontend/src/components/CommonFooter.jsx
@@ -13,54 +13,45 @@ function CommonFooter() {
   return (
     <Footer container className="bg-NeutralBlack text-FooterFont">
       <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1 ">
-          <div className="space-y-4 mb-8">
-            <div>
-              <p className="mb-1 text-FooterFont">Copyright © 2023 PMS,FoE,UoR</p>
-              <p >All rights reserved</p>
+        <div className="grid w-full justify-between sm:flex gap-8 sm:justify-between md:flex md:grid-cols-3  md:gap-14 text-left mt-4 mb-4">
+          <div className="space-y-4 mb-8 w-3/4 mx-4 my-12">
+            <div className="justify-center">
+              <p className="mb-1 text-FooterFont text-sm">
+                Copyright © 2023 PMS,FoE,UoR
+              </p>
+              <p className="text-sm">All rights reserved</p>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-2 sm:gap-6 text-center gap-4">
-            <div>
-              <a
-                href="/"
-                className="text-2xl font-semibold flex items-center space-x-3"
-              >
-                <span className="text-[#2194F3]">ENG</span>
-                <img
-                  src={logo}
-                  alt=" "
-                  className="w-10 inline-block item-center "
-                />
-                <span className="text-[#2194F3]">PMS</span>
-              </a>
-              <Footer.LinkGroup col>
-            {/*<Footer.Link href="#">The Faculty of Engineering of University of Ruhuna was established on 1st July 1999 at Hapugala, Galle. Admission to the Faculty of Engineering,
-              University of Ruhuna, is subject to the University Grants Commission policy on university admissions.</Footer.Link> */}
-              
-              </Footer.LinkGroup>
-            </div>
-            <div >
-              <Footer.Title title="Contact Us" className="text-FooterFont"/>
-              <Footer.LinkGroup col className="text-FooterFont">
-                <Footer.Link href="#">
-                  Faculty of Engineering, Hapugala, Galle, Sri Lanka.
-                </Footer.Link>
-                <Footer.Link href="#">Phone : +(94)0 91 2245765/6 </Footer.Link>
-                <Footer.Link>Email : webmaster@eng.ruh.ac.lk</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
+
+          <div className="justify-center mr-8">
+            <p
+              className="text-2xl font-semibold flex items-center space-x-1 mx-auto justify-center"
+            >
+              <span className="text-[#2194F3] text-sm">ENG</span>
+              <img
+                src={logo}
+                alt="FoE,UoR "
+                className="w-10 inline-block item-center "
+              />
+              <span className="text-[#2194F3] text-sm">PMS</span>
+            </p>
+
+            <p className="text-justify text-sm">
+              The Faculty of Engineering of University of Ruhuna was established
+              on 1st July 1999 at Hapugala, Galle. Admission to the Faculty of
+              Engineering, University of Ruhuna, is subject to the University
+              Grants Commission policy on university admissions.
+            </p>
           </div>
-        </div>
-        <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-         
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+          <div className="text-left items-center w-1/2 my-4 text-sm">
+            <p className="text-FooterFont font-bold">Contact Us</p><br/>
+            <p col className="text-FooterFont">
+              <ul>
+                <li>Faculty of Engineering, Hapugala, Galle, Sri Lanka.</li>
+                <li>Phone : +(94)0 91 2245765/6 </li>
+                <li>Email : <a href="mailto:webmaster@eng.ruh.ac.lk" className="text- underline">webmaster@eng.ruh.ac.lk</a></li>
+              </ul>
+            </p>
           </div>
         </div>
       </div>
