776beb840a60c329ee26cb183abb9f72f64ea6a1
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-03-03 16:16:42 +0530

    responsive pages

--- frontend/src/components/UserTypeNavbar.jsx
+++ frontend/src/components/UserTypeNavbar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; // Import Link from React Router
+// import { FaXmark, Fabars } from "react-icons/fa";
 
-import { FaXmark, FaBars } from "react-icons/fa";
+import { FaXmark, FaBars } from "react-icons/fa6";
 import "../styles/Navbar.css";
 import "../pages/Home";
 
