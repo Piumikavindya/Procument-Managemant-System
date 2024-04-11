d19254ed8f8eeb6d62995fe3239dc17beba5409f
ImashaNawodi <imashanaw1999@gmail.com>
2024-04-11 16:33:30 +0530

    commit

--- backend/Models/procReqest.js
+++ backend/Models/procReqest.js
@@ -1,20 +1,22 @@
-const mongoose = require("mongoose");
+
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-  filename: { type: String },
-  file: { type: String },
-  filepath: { type: String },
-});
-
-const itemSchema = new Schema({
-  itemId: {
-    type: String,
-    default: function () {
-      // Ensure the counter is initialized and incremented
-      this.constructor.counter = this.constructor.counter || 1;
-      return "Item" + String(this.constructor.counter++).padStart(3, "0");
-    },
+    filename: {type: String,},
+    file: {type: String,},
+    filepath:{type: String,},
+    
+  });
+  
+  const itemSchema = new Schema({
+    itemId: {
+      type: String,
+      default: function () {
+          // Ensure the counter is initialized and incremented
+          this.constructor.counter = this.constructor.counter || 1;
+          return 'Item' + String(this.constructor.counter++).padStart(3, '0');
+      },
   },
     itemName: {type: String,},
       cost: {type: Number,},
@@ -26,6 +28,7 @@ const itemSchema = new Schema({
 const procRequestSchema = new Schema({
   requestId: {
     type: String,
+    
     unique: true,
   },
   faculty: {type: String,},
@@ -38,13 +41,19 @@ const procRequestSchema = new Schema({
   balanceAvailable:{type: Number,},
   purpose:{
     type: String,
-    default: "normal",
-    enum: ["", "normal", "Fast Track", "Urgent", "Normal"],
+    default: 'normal',
+    enum: [ '','normal', 'Fast Track','Urgent','Normal']
   },
-  sendTo: {
+  sendTo:{
     type: String,
-    default: "dean",
-    enum: ["", "dean", "registrar", "viceChancellor"],
+    default: 'dean',
+    enum: ['','dean', 'registrar','viceChancellor']
+  },
+  
+  status: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected"],
+    default: "Pending",
   },
   items: [itemSchema],  // Array of items within ProcurementRequest schema
   files: [fileSchema],  // Array of files within ProcurementRequest schema
@@ -52,6 +61,6 @@ const procRequestSchema = new Schema({
 });
 
 
-const procReqest = mongoose.model("procRequest", procRequestSchema);
+const procReqest = mongoose.model('procRequest', procRequestSchema);
 
 module.exports = procReqest;
